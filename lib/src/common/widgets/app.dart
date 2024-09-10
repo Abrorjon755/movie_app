8603d3d281da7e780da872495911e9772abb0da6
Abrorjon755 <Abrorjonbahodirov@gmail.com>
2024-09-10 18:45:42 +0500

    free

--- lib/src/common/widgets/app.dart
+++ lib/src/common/widgets/app.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:movie_app/src/features/home/screen/home_screen.dart';
+
+import '../l10n/generated/l10n.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: Locale("en"),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale("en"),
+      ],
+      home: HomeScreen(),
     );
   }
 }
